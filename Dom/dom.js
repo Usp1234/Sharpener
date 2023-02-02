2457c5636fbc96a428c2b3a05b5da4defda7d4b6
Usp1234 <priyankabebarta58>
2023-02-02 18:02:32 +0530

    add

--- Dom/dom.js
+++ Dom/dom.js
@@ -55,16 +55,77 @@
 // secondItem.style.color='coral';
  
 // QUERYSELECTORFORALL
-var titles=document.querySelectorAll('.title');
+// var titles=document.querySelectorAll('.title');
 
-console.log(titles);
-titles[0].textContent='Hello';
+// console.log(titles);
+// titles[0].textContent='Hello';
 
-var odd=document.querySelectorAll('li:nth-child(odd)');
-var even=document.querySelectorAll('li:nth-child(even)');
+// var odd=document.querySelectorAll('li:nth-child(odd)');
+// var even=document.querySelectorAll('li:nth-child(even)');
 
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='#f4f4f4';
-    even[i].style.backgroundColor='#ccc';
-}
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor='#f4f4f4';
+//     even[i].style.backgroundColor='#ccc';
+// }
 
+// TRAVERSING THE DOM
+var itemList=document.querySelector('#items');
+
+// // parentnode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor='#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+// parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor='#f4f4f4';
+// console.log(itemList.parentElement.parentElement);
+
+// childNodes
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor='yellow';
+
+// firstChild
+// console.log(itemList.firstChild);
+// firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent='Hello 1';
+
+// Lastchild
+// console.log(itemList.lastChild);
+// lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent='Hello 4';
+
+// nextsibling
+// console.log(itemList.nextSibling);
+// nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// previousSibling
+// console.log(itemList.previousSibling);
+// previousElementSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.getElementsByClassName.color='green';
+
+// createElement
+// create a div
+// var newDiv=document.createElement('div');
+
+// add class
+// newDiv.className='hello';
+
+// add id
+// newDiv.id='hello 1';
+
+// add Attr
+// newDiv.setAttribute('title','hello div');
+
+// create text Node
+// var newDivText=document.createTextNode('Hello world');
+
+// add text to Div
+// newDiv.appendChild(newDivText);
+
+// console.log(newDiv);
